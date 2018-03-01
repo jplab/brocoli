5fdc8829bfc46fa2a99f4fd26f8e557e6455680c
Jean-Philippe Labbé <labbe@math.fu-berlin.de>
2018-03-01 16:35:19 +0100

    Added version check

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys
 from distutils.core import setup
 from codecs import open # To open the README file with proper encoding
 from setuptools.command.test import test as TestCommand # for tests
+from distutils.command import build as build_module
 
 
 # Get information from separate files (README, VERSION)
@@ -11,12 +12,24 @@ def readfile(filename):
     with open(filename,  encoding='utf-8') as f:
         return f.read()
 
-# # For the tests
+# Check the right Sage version
+class build(build_module.build):
+    def run(self):
+        from sagemath.check_version import check_version
+        check_version(sage_required_version)
+        build_module.build.run(self)
+
+# For the tests
 class SageTest(TestCommand):
     def run_tests(self):
         errno = os.system("sage -t --force-lib brocoli")
         if errno != 0:
             sys.exit(1)
+
+# Specify the required Sage version
+sage_required_version = '>=8.1'
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines() if i[0] != '#']
+CLASSIFIERS = [i.strip() for i in open("classifiers.txt").readlines() if i[0] != '#']
 
 setup(
     name = "brocoli",
@@ -41,5 +54,5 @@ setup(
     ], # classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords = "SageMath",
     packages = ['brocoli'],
-    cmdclass = {'build': build, 'test': SageTest} # adding a special setup command for tests
+    cmdclass = {'test': SageTest} # adding a special setup command for tests
 )
