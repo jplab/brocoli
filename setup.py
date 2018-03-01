8a6b4231e6b2ac23cd3b6489b66a2f65291f6393
Jean-Philippe Labbé <labbe@math.fu-berlin.de>
2018-03-01 16:09:23 +0100

    Added a line in setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 import sys
 from distutils.core import setup
 from codecs import open # To open the README file with proper encoding
+from setuptools.command.test import test as TestCommand # for tests
 
 
 # Get information from separate files (README, VERSION)
